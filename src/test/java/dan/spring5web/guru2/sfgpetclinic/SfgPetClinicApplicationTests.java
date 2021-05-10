aad3d5e9ddad9d0f296b974b0a6d48ed15b91ab7
Daniyar Burakaev <daniyar.burakaev@mail.ru>
2021-05-10 11:20:07 +0300

    Initial Project

---
+++ src/test/java/dan/spring5web/guru2/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.spring5web.guru2.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
